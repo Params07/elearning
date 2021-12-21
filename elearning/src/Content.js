b0d3fd512bb4f6f7907fed3aa1f26bc96feed672
Params07 <92327167+Params07@users.noreply.github.com>
2021-12-21 23:46:25 +0530

    Update Content.js

--- elearning/src/Content.js
+++ elearning/src/Content.js
@@ -149,7 +149,7 @@ const Content =()=>
       <div className="mt-4 ... pr-0  sm:pr-8 ...">
             <div className=" grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3  space-around">
         {con.map(content =>(
-              <List  title={content['title']}   url = {content['url']} trans = {checkbox}/>
+              <List  title={content['title']} des={content['description']}  url = {content['url']} trans = {checkbox}/>
               
             ))}
             </div>
