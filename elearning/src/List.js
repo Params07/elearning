825751aa90576a23077251975461d5ec0d69c539
Params07 <92327167+Params07@users.noreply.github.com>
2021-12-21 23:48:14 +0530

    Update List.js

--- elearning/src/List.js
+++ elearning/src/List.js
@@ -16,7 +16,9 @@ const List = ({title,des,url})=>
     <ReactPlayer  width="100%" height="100%"  url={url}  controls />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2 hover:underline uppercase ...">{title}</div>
-        
+        <p className="pl-4 ... text-gray-500 text-base">
+         {des}
+        </p>
       </div>
      
        
